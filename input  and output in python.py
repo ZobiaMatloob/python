fd9a3739908c2e910c58cddd0e0f5af3778b1fec
Zobia Matloob <zobiamatloob1@gmail.com>
2026-08-25 18:54:14 +0500

    Add files via upload

---
+++ input  and output in python.py
@@ -0,0 +1,31 @@
+
+# Here we are learing input and output in python.
+
+x = input("Enter first number:")  
+y = input("Enter second number:") 
+x = int(x)
+y = int(y)
+sum = x+y
+print("sum is", sum)
+
+
+#or it can be written as:
+x = int(input("Enter first number:"))
+y = int(input("Enter second number:"))
+sum = x+y
+print("sum is",sum)
+
+# the important thing to be noted is that the input function in which we give input is input function but the out put function displays text on screen and 
+# out put function also called as print function. They are same as we write out first program of hello world that is the example of output function.
+
+
+age = input("Enter your age:")
+potential = float(input("Enter your potential:"))
+print("your age and potential is", age,potential)
+
+
+# Now the program i have written above is called type casting it means that we can convert it to any form like int,float or bool
+# In the written float We can change the user input from default string type to any other type (int, float, etc) by typecasting.
+#Another thing to remember is the age and potential are just variables and we can also write them as a,p or x, y,z.
+
+
