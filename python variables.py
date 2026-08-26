a30fca6d0078ba602496dad97429d8273b507fbe
Zobia Matloob <zobiamatloob1@gmail.com>
2026-08-26 12:52:46 +0500

    Add files via upload

---
+++ python variables.py
@@ -0,0 +1,22 @@
+#Question no 1
+celcius = 10
+fahrenheit = (celcius*9/5)+32
+print(fahrenheit)
+
+
+# Question no 2
+username = "anna"
+age = 30 
+account_balance = 70000
+is_active = True
+
+print(username)
+print(age)
+print(account_balance)
+print(is_active)
+# Here username, age, account_balance and is_active are the variables which are used to store the data
+
+# To find the length of the variables we use the code like this 
+word = "Python"
+length = len(word)
+print("Length of the word:", length)
