547691908fd5fb1b007406fe9f2b4c9d106f693d
Zobia Matloob <zobiamatloob1@gmail.com>
2026-08-28 17:31:43 +0500

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,19 @@
+# Data types in Python
+
+x = type(12)                                   #integer
+y = type(20.5)                                 #float
+k = type(1j)                                   #complex
+z = type("Hello World")                        #string
+l = type(True)                                 #boolean
+j = type(["berry", "strawberry", "grape"])     #list
+g = type({'apple', 'banana', 'cherry'})        #set
+a = type({'name': 'John', 'age': 36})          #dictionary
+
+print(x)
+print(y)
+print(k)
+print(z)
+print(l)
+print(j)
+print(g)
+print(a)
