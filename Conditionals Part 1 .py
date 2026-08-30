0dd3372b57151fe6fe83539d417971f4390a274a
Zobia Matloob <zobiamatloob1@gmail.com>
2026-08-30 20:45:38 +0500

    Add files via upload

---
+++ Conditionals Part 1 .py
@@ -0,0 +1,24 @@
+# if statements are used to make decisions in programming. 
+# They allow the program to execute certain blocks of code based on conditions.
+
+#Example 1:
+if True:
+    print("This will always execute because the condition is True")
+
+#iF else statements are used to provide an alternative block of code to execute
+#  if the condition is False.
+
+#Example 1:
+you = "you can protest"
+if you:
+    print("you can protest")
+else :
+    print("you cannot protest")
+
+#Example 2:
+age = 18
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
