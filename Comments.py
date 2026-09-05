3f9ca4ab90a3a75b1521c34daf670201c997540d
Zobia Matloob <zobiamatloob1@gmail.com>
2026-09-05 21:44:38 +0500

    Add files via upload

---
+++ Comments.py
@@ -0,0 +1,16 @@
+# comments are used to explain code and make it more
+#  readable.
+#  They are ignored by the Python interpreter.
+# they are created by using the hash (#) symbol
+#  before the comment text.
+
+#Example 1:
+print("Hello, World!")  # This will print "Hello, World!" to the console
+
+#Example 2:
+x = 5  # This will assign the value 5 to the variable x
+print(x)  # This will print the value of x to the console
+
+# Example 3:
+#This is a comment
+print("Hi!")
