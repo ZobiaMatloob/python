ac2476557ba6016c84e7e271f197f39955ad3ad4
Zobia Matloob <zobiamatloob1@gmail.com>
2026-09-06 19:57:40 +0500

    Add files via upload

---
+++ python casting.py
@@ -0,0 +1,28 @@
+# Type casting in Python
+# Type casting is the process of converting a variable from one data type to another.
+#  In Python, you can perform type casting using built-in functions.
+
+#Example no 1: Converting an integer to a float
+x = 10
+y = float(x)
+print(y)
+
+#Example no 2: Converting a float to an integer
+z = 10.5
+w = int(z)
+print(w)
+
+#Example no 3: Converting a string to an integer
+s = "20"
+t = int(s)
+print(t)
+
+#Example no 4: Converting a string to a float
+s2 = "30.5"
+t2 = float(s2)
+print(t2)
+
+#Example no 5: Converting an integer to a string
+num = 100
+str_num = str(num)
+print(str_num)
