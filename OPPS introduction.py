e7c73272d384d626057a57b8aa66c4a21c836c3e
Zobia Matloob <zobiamatloob1@gmail.com>
2026-09-23 20:12:07 +0500

    Add files via upload

---
+++ OPPS introduction.py
@@ -0,0 +1,22 @@
+# OPPS stands for object-oriented programming.
+# Classes and objects are the two core concepts in object-oriented programming.
+
+# A class defines what an object should look like, and an object is created based on that class.
+ 
+# Example 1: 
+
+languages =  ["Urdu", "English", "French"]
+print(languages) 
+
+# Explanatiom: In the above example, we have created a list of languages.
+# The variable languages is a class and urdu , english and french are the objects.
+
+# Example 2:
+
+color = [ "Green", "Blue", "Red"]
+print(color)
+
+# Here the variable color is the class and thr color names are the objects.
+# and the strings green, blue and red are the objects
+
+
