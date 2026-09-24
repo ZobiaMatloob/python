f9666e85297e94528984fb7a53c353bef3626fe8
Zobia Matloob <zobiamatloob1@gmail.com>
2026-09-24 20:59:17 +0500

    Add files via upload

---
+++ classes and objects.py
@@ -0,0 +1,24 @@
+# creating classes and objects
+
+# Example 1 :
+
+class MyClass:
+ x = 7
+c1 = MyClass()
+print(c1.x)  #output:7
+
+# deleteing object
+del c1
+
+# Creating multiple objects
+
+o1 = MyClass()
+o2 = MyClass()
+o3 = MyClass()
+
+print(o1.x)
+print(o2.x)
+print(o3.x)
+
+
+
